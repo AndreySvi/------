82fd3915124b6185c39223f4fb2fa10b49a8ee57
AndreySvi <141420163+AndreySvi@users.noreply.github.com>
2023-08-09 22:10:13 +0300

    второй способ обработки json

--- script/script.js
+++ script/script.js
@@ -50,6 +50,30 @@ function dataHandlerWeather(data) {
 	}
 }
 
+// альтернативый способ работы с json
+/*async function weatherData-test() {
+	const res = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c46d10d1c142ac6c1cfe635c49e83765`
+	).then((Response) => Response.json());
+
+	return res;
+}
+
+async function dataHandlerWeather() {
+	let data = await weatherData-test();
+	if (data.cod == 200) {
+		backgroundImage(data.name);
+		temp.innerText = `${(data.main.temp - 273.15).toFixed(2)}°C`;
+		humidity.innerText = `humidityBy: ${data.main.humidity}%`;
+		windSpeed.innerText = `wind Speed: ${data.wind.speed}km/h`;
+		cloudiness.innerText = `${data.weather[0].description}`;
+		weatherLocation.innerText = ` ${data.name}`;
+		imgCloudiness.src = `https://openweathermap.org/img/wn/${
+			data.weather[0].icon + '@2x.png'
+		}`;
+	}
+}*/
+
 function backgroundImage(city) {
 	fetch(
 		`https://api.unsplash.com/search/photos?query=${city}&client_id=5Qh9rBeWz9My7VQZPJCAZO_TppyHIH_n853s4dqU44w`
